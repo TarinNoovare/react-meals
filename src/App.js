3d6122cd6403e3afa6d360e289d2b50efe940db2
Tarin Noovare <tarin.no@tnis.com>
2022-04-13 11:25:01 +0700

    Adjust CartContext to use raw menu script

--- src/App.js
+++ src/App.js
@@ -5,13 +5,49 @@ import { Menu } from "./Menu/Menu";
 import CartContext from "./context/cart-context";
 
 function App() {
-  const numberOfCart = 0;
+  const menuList = [
+    {
+      id: 0,
+      title: "Sushi",
+      phrase: "Delicious Shushi U wanna Try",
+      price: 28.99,
+      amount: 0,
+    },
+    {
+      id: 1,
+      title: "Fried Rice",
+      phrase: "Rice with Fried, You know it...",
+      price: 10.99,
+      amount: 0,
+    },
+    {
+      id: 2,
+      title: "Green Bowl",
+      phrase: "Healthy and Green",
+      price: 18.99,
+      amount: 0,
+    },
+    {
+      id: 3,
+      title: "Barbecue Burger",
+      phrase: "American, Raw, Meaty",
+      price: 12.99,
+      amount: 0,
+    },
+    {
+      id: 4,
+      title: "Schnitzel",
+      phrase: "A German Specialty",
+      price: 16.99,
+      amount: 0,
+    },
+  ];
 
   return (
     <div className="App">
-      <CartContext.Provider value={{ numberOfCart }}>
+      <CartContext.Provider value={{ menuList }}>
         <NavBar />
-        <OpenPhrase/>
+        <OpenPhrase />
         <Menu />
       </CartContext.Provider>
     </div>
